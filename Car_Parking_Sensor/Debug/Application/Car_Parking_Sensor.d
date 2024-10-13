74bf3697b97ce2286e4885f02ab27d26ed7eca1f
M0oham6d <mo321343@gmail.com>
2024-10-13 09:54:28 +0300

    Car Parking Sensor

---
+++ Car_Parking_Sensor/Debug/Application/Car_Parking_Sensor.d
@@ -0,0 +1,33 @@
+Application/Car_Parking_Sensor.o Application/Car_Parking_Sensor.o:  \
+ ../Application/Car_Parking_Sensor.c ../Application/Car_Parking_Sensor.h \
+  ../Application/../HAL/buzzer.h ../Application/../HAL/../MCAL/gpio.h \
+  ../Application/../HAL/../MCAL/../Standard_Types/standard_types.h \
+  ../Application/../HAL/../MCAL/../Common_Macros/common_macros.h \
+  ../Application/../HAL/lcd.h \
+  ../Application/../HAL/../Standard_Types/standard_types.h \
+  ../Application/../HAL/../Common_Macros/common_macros.h \
+  ../Application/../HAL/three_leds.h \
+  ../Application/../HAL/ultrasonic_sensor.h \
+  ../Application/../HAL/../MCAL/icu.h
+
+../Application/Car_Parking_Sensor.h:
+
+../Application/../HAL/buzzer.h:
+
+../Application/../HAL/../MCAL/gpio.h:
+
+../Application/../HAL/../MCAL/../Standard_Types/standard_types.h:
+
+../Application/../HAL/../MCAL/../Common_Macros/common_macros.h:
+
+../Application/../HAL/lcd.h:
+
+../Application/../HAL/../Standard_Types/standard_types.h:
+
+../Application/../HAL/../Common_Macros/common_macros.h:
+
+../Application/../HAL/three_leds.h:
+
+../Application/../HAL/ultrasonic_sensor.h:
+
+../Application/../HAL/../MCAL/icu.h:
---
+++ Car_Parking_Sensor/Debug/HAL/three_leds.d
@@ -0,0 +1,12 @@
+HAL/three_leds.o HAL/three_leds.o: ../HAL/three_leds.c \
+  ../HAL/three_leds.h ../HAL/../MCAL/gpio.h \
+  ../HAL/../MCAL/../Standard_Types/standard_types.h \
+  ../HAL/../MCAL/../Common_Macros/common_macros.h
+
+../HAL/three_leds.h:
+
+../HAL/../MCAL/gpio.h:
+
+../HAL/../MCAL/../Standard_Types/standard_types.h:
+
+../HAL/../MCAL/../Common_Macros/common_macros.h:
